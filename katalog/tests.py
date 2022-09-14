af16a6dbcdded021c3a8b121963a8e9c89e4dc51
Hugo Sulaiman Setiawan <hugo.sulaiman@ui.ac.id>
2022-09-14 11:28:12 +0700

    Add unit tests

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import resolve
+from .views import show_katalog
 
 # Create your tests here.
+class KatalogAppTest(TestCase):
+    def test_katalog_app_url_routed_and_responds(self):
+        response = Client().get('/katalog/')
+        self.assertEqual(response.status_code,200)
+
+    def test_katalog_app_using_katalog_template(self):
+        response = Client().get('/katalog/')
+        self.assertTemplateUsed(response, 'katalog.html')
