55dc888dd75facb02012322676f019d81e8c96f7
Hugo Sulaiman Setiawan <hugo.sulaiman@ui.ac.id>
2022-09-14 10:50:58 +0700

    Implement show_katalog view

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,16 @@
+from multiprocessing import context
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_katalog(request):
+    # get data from BarangKatalog
+    data_item_catalog = CatalogItem.objects.all()
+
+    context = {
+        'list_item_catalog': data_item_catalog,
+        'nama_student': 'Hugo Sulaiman Setiawan',
+        'id_student': '2106707315'
+    }
+
+    # call render function with given data context
+    return render(request, "katalog.html", context)
