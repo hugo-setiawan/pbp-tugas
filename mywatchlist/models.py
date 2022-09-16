66717cfcd6691b94fd37a6496d091b452969536e
Hugo Sulaiman Setiawan <hugo.sulaiman@ui.ac.id>
2022-09-17 00:00:34 +0700

    Add watchlist model, migration, and data

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class MyWatchList(models.Model):
+    watched = models.BooleanField()
+    title = models.TextField()
+    rating = models.PositiveSmallIntegerField()
+    release_date = models.DateField()
+    review = models.TextField()
