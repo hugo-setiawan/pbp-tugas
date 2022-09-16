e26fb7b4c023e080d3a30a46e88e311e8fdd5d49
Hugo Sulaiman Setiawan <hugo.sulaiman@ui.ac.id>
2022-09-17 00:13:55 +0700

    Add unit tests for watchlist
    Unit tests to check whether each of the three URLs respond with 200

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+class MyWatchlistAppTest(TestCase):
+    def test_watchlist_html_response_ok(self):
+        response = Client().get("/mywatchlist/html/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_watchlist_xml_response_ok(self):
+        response = Client().get("/mywatchlist/xml/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_watchlist_json_response_ok(self):
+        response = Client().get("/mywatchlist/json/")
+        self.assertEqual(response.status_code, 200)
