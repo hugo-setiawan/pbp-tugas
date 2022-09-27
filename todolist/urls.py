b8cf380f246a8f26670e6ebeeaf6731ea603e005
Hugo Sulaiman Setiawan <hugo.sulaiman@ui.ac.id>
2022-09-27 22:36:00 +0700

    Implement user registration and login

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from todolist.views import register_user, login_user
 
 app_name = 'todolist'
 
-urlpatterns = []
+urlpatterns = [
+    path('register/', register_user, name='register_user'),
+    path('login/', login_user, name='login_user'),
+]
--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+import datetime
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+def show_todolist(request):
+    pass # TODO
+
+def register_user(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Akun telah berhasil dibuat!')
+            return redirect('todolist:login_user')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            response =  HttpResponseRedirect(reverse('todolist:show_todolist'))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+
+        else:
+            messages.info(request, 'Username atau Password anda salah!')
+    
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('todolist:login_user'))
+    response.delete_cookie('last_login')
+    return response
