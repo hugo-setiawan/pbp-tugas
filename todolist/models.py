bbab20b97c9329e964c35c8a2e208d24f742f935
Hugo Sulaiman Setiawan <hugo.sulaiman@ui.ac.id>
2022-09-27 22:13:36 +0700

    Create Task model and migration

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    date = models.DateField()
+    title = models.TextField()
+    description = models.TextField()
